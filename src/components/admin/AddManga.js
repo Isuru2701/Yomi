ab6d9057877b72cb8142ef1b4329de6f239a9655
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-04-18 07:15:25 +0530

    added skeleton

---
+++ src/components/admin/AddManga.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AddManga() {
+    /* repurpose this as edit manga info by filling in the fields when editing */
+    return (
+        <div>
+        <h1>Add Manga</h1>
+        </div>
+    );
+}
+
