91b4ed6d4d46c7e667de39231a8fab643da5cbfb
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-04-18 11:06:58 +0530

    login done

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,46 @@ import React from "react";
 
 export default function Login() {
     return (
-        <div>
-        
+        <div className="flex w-full h-screen">
+            <img src="/login_cover.jpg" className="object-cover h-full w-full" alt="image background" />
+            <div className="absolute inset-0 bg-gradient-to-l from-transparent to-black"></div>
+
+            <div className="absolute inset-y-0 right-0 w-1/3 bg-background bg-opacity-100 flex flex-col items-center justify-center  rounded-l-lg mt-4 mb-4 mr-8">
+                <h1 className="text-7xl">Login</h1>
+
+                <br />
+                <br />
+                <form className="flex flex-col space-y-5 w-2/3 ">
+                    <input
+                        type="text"
+                        className="px-10 py-6 border border-gray-600 bg-background rounded-md text-gray-100 focus:outline-none focus:border-highlight"
+                        placeholder="Email"
+                        required={true}
+
+                    />
+                    <br />
+
+                    <input
+                        type="password"
+                        className="px-10 py-5 border border-gray-600 rounded-md bg-background text-gray-100 focus:outline-none focus:border-highlight"
+                        placeholder="Password"
+                    />
+
+                    <br />
+                    <input
+                        type="submit"
+                        className="px-4 py-5 bg-highlight text-white rounded-md mt-4 cursor-pointer"
+                        value="Login"
+                    />
+                </form>
+            
+            </div>
+            <div className="absolute bottom-0 left-0 p-4 text-gray-100 text-sm">
+                    © Artwork by <a href="https://www.pixiv.net/en/artworks/95148272">粉帮老大</a>
+                </div>
+
+
+
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary : "#711db0",
+        secondary: "#c21292",
+        highlight: "#ef4040",
+        warm: "#ffa732",
+        success: "#98EA97",
+        danger: "#EA9797",
+        background: "#1F1F1F"
+      }  
+    },
   },
   plugins: [],
 }
